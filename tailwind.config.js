aaa0eb537f212332805273480040cb8188e30730
Muhammad Basiq Khan <khanbasiq16@gmail.com>
2025-07-05 23:12:39 +0500

    feat: Add new components for FloatingImage, Footer, Hero, Navbar, VideoPreview, and update styles
    - Implemented FloatingImage component with GSAP animations for mouse movement effects.
    - Created Footer component with social media links and privacy policy.
    - Developed Hero component featuring video previews and interactive elements.
    - Added Navbar component with audio toggle functionality and navigation links.
    - Introduced VideoPreview component for enhanced video interaction.
    - Updated global styles and added Tailwind CSS configuration.
    - Removed unused page.module.css and refactored main page structure.

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+       fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
+      },
+       colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
